6c8724ea61b1222a4462671a586422cfb655e1db
ars <ars.anosov@gmail.com>
2018-03-06 14:31:08 +0300

    inline keyboard

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ const sslCredentials = {
   ca:   fs.readFileSync( path.join(__dirname, 'cert/ca.pem') )
 }
 
+const request = require('request');
+var reqOptions = {  
+  url:      'http://xn--80ahqgegdcb.xn--p1ai/newtest.php',
+  method:   'POST',
+  encoding: 'utf8'
+}
+
 
 
 // Set telegram webhook (upload cert)
@@ -62,14 +69,23 @@ httpsServer.listen(whPort, () => {
 
 // context extend ------------------------------------------------------------
 bot.context.localDb = [
-
+  { id: 65111617,
+    is_bot: false,
+    first_name: 'Arseny',
+    last_name: 'Anosov',
+    username: 'ars_anosov',
+    language_code: 'ru',
+    do: {
+      id: '01810413042017'
+    }
+  }
 ]
 
 
-//const Router = require('telegraf/router')
 const session = require('telegraf/session')
-//const Extra = require('telegraf/extra')
-const Markup = require('telegraf/markup')
+const Router = require('telegraf/router')
+const Extra = require('telegraf/extra')
+//const Markup = require('telegraf/markup')
 //
 //const Stage = require('telegraf/stage')
 //const Scene = require('telegraf/scenes/base')
@@ -85,32 +101,38 @@ bot.use(async (ctx, next) => {
   const start = new Date()
   await next()
   const ms = new Date() - start
+  console.log('timer middleware ------------------------:')
   console.log('Response time %sms', ms)
 })
 
 // Naive authorization middleware
 bot.use((ctx, next) => {
   ctx.state.role = false
-  ctx.localDb.map( (row, i) => {
-    if (row.id === ctx.message.from.id) { ctx.state.role = row }
-  })
+  let objRef = {}
+
+  if (ctx.message)                { objRef = ctx.message }
+  if (ctx.update.callback_query)  { objRef = ctx.update.callback_query }
+
+  console.log('role middleware ------------------------:')
+  console.log(objRef)
+  if (objRef.from) {
+    ctx.localDb.map( (row, i) => {
+      if (row.id === objRef.from.id) { ctx.state.role = row }
+    })
+  }
+
   return next()
 })
 
 // session
-bot.use(session())
+bot.use(session({ ttl: 10 }))
 
 
 
 
 
 
-const request = require('request');
-var reqOptions = {  
-  url:      'http://xn--80ahqgegdcb.xn--p1ai/newtest.php',
-  method:   'POST',
-  encoding: 'utf8'
-}
+
 
 
 
@@ -131,65 +153,165 @@ var reqOptions = {
 
 // curl -d "request_type=SRGP_API_DOG_BALANCE&dog_id=01234513042017" -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" -X POST http://xn--80ahqgegdcb.xn--p1ai/newtest.php
 
-const l1_1        = '\u26F3 Я абонент домонлайн.рф'
-const l1_2        = '\u26F9 Я хочу стать абонентом'
-const l1_1_l2_1   = 'Проверить баланс'
+
+const id_input_text = 'Напишите свой <b>ID</b>\n(числовое значение, полученное Вами при включении)'
+const id_change_text = 'Ok. Напишите новый ID.'
 
 
-bot.start((ctx) => {
-  ctx.reply(
-    'Поздравляем '+ctx.message.from.username+'! Вы подписались на домонлайн.рф.\nИспользуйте /off чтобы приостановить подписку.',
-    Markup
-    .keyboard([
-      [l1_1, l1_2]
-    ])
-    .oneTime()
-    .resize()
-    .extra()
-  )
+const level_1_markup = Extra
+  .HTML()
+  .markup((m) => m.inlineKeyboard([
+    m.callbackButton('\u26F3 Я абонент домонлайн.рф', 'abonent:123'),
+    m.callbackButton('\u26F9 Я хочу стать абонентом', 'not_abonent:456')
+  ], {columns: 2}))
+
+
+const level_2_markup = Extra
+  .HTML()
+  .markup((m) => m.inlineKeyboard([
+    m.callbackButton('\u26FD Проверить баланс', 'balance_check:456'),
+    m.callbackButton('\u267B У меня другой ID', 'id_change:123'),
+    m.callbackButton('\u2693 Start', 'go_start:456')
+  ], {columns: 2}))
+
+const level_last_markup = Extra
+  .HTML()
+  //.markup((m) => m.inlineKeyboard([
+  //  m.callbackButton('\u2693 Start', 'go_start:456')
+  //], {columns: 1}))
+
+
+
+
+
+
+
+
+
+const callbackRouter = new Router(({ callbackQuery }) => {
+  if (!callbackQuery.data) {
+    return
+  }
+
+  console.log('Router callbackQuery ------------------------:')
+  console.log(callbackQuery)
+  const parts = callbackQuery.data.split(':')
+  return {
+    route: parts[0],
+    state: {
+      amount: parseInt(parts[1], 10) || 0
+    }
+  }
 })
 
-
-
-bot.hears(l1_1, ctx => {
-  
+callbackRouter.on('abonent', (ctx) => {
   if (ctx.state.role) {
-    ctx.reply(
-      'Ваш ID '+ctx.state.role.do.id,
-      Markup
-      .keyboard([
-        [l1_1_l2_1, '/start']
-      ])
-      .oneTime()
-      .resize()
-      .extra()
-    )
+    ctx.session.value = 'Ваш ID: <b>'+ctx.state.role.do.id+'</b>'
+    ctx.editMessageText(ctx.session.value, level_2_markup).catch(() => undefined)
   }
   else {
-    ctx.reply('Введите свой ID (числовое значение, полученное Вами при включении)')
+    ctx.session.value = id_input_text
+    ctx.editMessageText(ctx.session.value, level_last_markup).catch(() => undefined)
   }
-
 })
 
+callbackRouter.on('not_abonent', (ctx) => {
+  ctx.session.value = 'В разработке...'
+  ctx.editMessageText(ctx.session.value, level_1_markup).catch(() => undefined)
+})
 
+callbackRouter.on('id_change', (ctx) => {
+  ctx.session.value = id_change_text
+  ctx.editMessageText(ctx.session.value, level_last_markup).catch(() => undefined)
+})
 
+callbackRouter.on('balance_check', (ctx) => {
 
-bot.hears(l1_1_l2_1, (ctx) => {
   let reqOp = {...reqOptions}
   reqOp.form = {request_type: 'SRGP_API_DOG_BALANCE', dog_id: ctx.state.role.do.id}
   
-  let resultJson = ['x', 'x']
   request(reqOp, (requestErr, requestRes, requestBody) => {
-    resultJson = JSON.parse(requestBody)
-    ctx.reply('Ваш баланс: '+resultJson[0]+' \u20BD')
-    ctx.reply('Оплачено дней: '+resultJson[1])
+    let resultJson = JSON.parse(requestBody)
+    ctx.session.value = 'Ваш баланс: <b>'+resultJson[0]+' \u20BD</b>\nОплачено дней: <b>'+resultJson[1]+'</b>'
+    //console.log(ctx.session.value)
+    ctx.editMessageText(ctx.session.value, level_2_markup).catch(() => undefined)
   })
-  ctx.reply('Запрос данных...')
+
+  ctx.editMessageText(new Date(), level_2_markup).catch(() => undefined)
 })
 
-
-
-bot.hears(l1_2, ctx => {
-  ctx.reply('Превращаемся в абонента.')
+callbackRouter.on('go_start', (ctx) => {
+  ctx.session.value = 'start'
+  ctx.editMessageText(ctx.session.value, level_1_markup).catch(() => undefined)
 })
 
+callbackRouter.otherwise((ctx) => ctx.reply('🌯'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bot.start((ctx) => {
+  ctx.session.value = 'start'
+  return ctx.reply(ctx.session.value, level_1_markup)
+})
+bot.on('callback_query', callbackRouter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bot.hears(/.*/, (ctx) => {
+  console.log('<--------------- hears ---------------->')
+
+  if (ctx.session.value === id_input_text) {
+    let exists = false
+    ctx.localDb.map( (row, i) => {
+      if (row.id === ctx.message.from.id) { exists = i }
+    })
+    if (!exists) {
+      ctx.message.from.do = {id: ctx.message.text}
+      bot.context.localDb.push(ctx.message.from)
+    }
+
+    ctx.session.value = 'Успешно! ID присвоен.'
+    ctx.reply(ctx.session.value, level_1_markup)
+  }
+
+  if (ctx.session.value === id_change_text) {
+    ctx.localDb.map( (row, i) => {
+      if (row.id === ctx.message.from.id) {
+        bot.context.localDb[i].do.id = ctx.message.text
+      }
+    })
+
+    ctx.session.value = 'Успешно! ID изменен.'
+    ctx.reply(ctx.session.value, level_1_markup)
+  }
+  
+})
