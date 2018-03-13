f0a29d419de2cc5bcd23025606c75354e7034cae
ars <ars.anosov@gmail.com>
2018-03-13 17:13:59 +0300

    Платежи работают

--- index.js
+++ index.js
@@ -270,32 +270,37 @@ callbackRouter.on('balance_pay', (ctx) => {
 
   const invoice = {
     provider_token: ykToken,
-    start_parameter: 'start_parameter_test',
-    title: 'Абонент ID '+ctx.state.role.do.id,
-    description: 'пополнение баланса',
+    start_parameter: 'test',
+    title: 'Абон. плата',
+    description: 'Абонент '+ctx.state.role.do.id,
     currency: 'RUB',
-    is_flexible: false,   // true если есть shipping method
+    //photo_url: 'http://xn--80ahqgegdcb.xn--p1ai/assets/images/logo.png',
+    is_flexible: false,   // true if shipping method
     prices: [
-      { label: 'RUB', amount: 6000 }
+      { label: 'Абон. плата', amount: 11000 }
     ],
-    payload: 'Абонент ID '+ctx.state.role.do.id+' - пополнение баланса',
-    provider_data: {
-      receipt: {
-        email: 'ars-anosov@yandex.ru',
-        items: [
-          {
-            description: 'Абонент ID '+ctx.state.role.do.id,
-            quantity: '1.00',
-            amount: {
-              value: '60.00',
-              currency: 'RUB'
-            },
-            vat_code: 1
-          }
-        ]
-      }
+    payload: {
+      item: 'Абон. плата'
     }
+    //provider_data: {
+    //  receipt: {
+    //    email: 'ars-anosov@yandex.ru',
+    //    items: [
+    //      {
+    //        description: 'ID '+ctx.state.role.do.id,
+    //        quantity: '1.00',
+    //        amount: {
+    //          value: '110.00',
+    //          currency: 'RUB'
+    //        },
+    //        vat_code: 1
+    //      }
+    //    ]
+    //  }
+    //}
   }
+
+  console.log(invoice)
 
   //ctx.telegram.sendInvoice(ctx.message.chat.id, invoice)
   ctx.replyWithInvoice(invoice)
@@ -332,6 +337,67 @@ callbackRouter.on('issues', (ctx) => {
 
 callbackRouter.on('pay_methods', (ctx) => {
   ctx.session.value = 'Способы оплаты (в разработке)'
+//'            <a href="https://kassa.yandex.ru/payments#cards" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-1.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#cards" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-2.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#cards" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-3.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#cards" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-4.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#cards" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-22.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#emoney" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-5.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#emoney" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-6.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#emoney" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-7.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#bank" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-8.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#bank" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-9.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#bank" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-10.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#bank" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-11.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#phone" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-12.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#phone" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-13.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#phone" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-14.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#phone" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-21.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#credit" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-16.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#cash" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-18.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#cash" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-19.png" alt="Alt">\n'+
+//'            </a>\n'+
+//'            <a href="https://kassa.yandex.ru/payments#cash" target="_blank" class="payment__item">\n'+
+//'              <img src="http://xn--80ahqgegdcb.xn--p1ai/assets/images/pay-icons/pay-20.png" alt="Alt">\n'+
+//'            </a>\n'
+
   ctx.editMessageText(ctx.session.value, level_2_2_markup).catch(() => undefined)
 })
 
@@ -367,10 +433,21 @@ bot.start((ctx) => {
 })
 bot.on('callback_query', callbackRouter)
 
+bot.on('shipping_query', ({ answerShippingQuery }) => answerShippingQuery(true, []))
+
 bot.on('pre_checkout_query', (ctx) => {
   console.log('on - pre_checkout_query')
   ctx.answerPreCheckoutQuery(true)
+  .then((state) => {
+    console.log('answerPreCheckoutQuery - Ok')
+    console.log(state)
+  })
+  .catch((error) => {
+    console.log('answerPreCheckoutQuery - Err')
+    console.log(error)
+  })
 })
+
 bot.on('successful_payment', () => console.log('on - successful_payment'))
 
 
