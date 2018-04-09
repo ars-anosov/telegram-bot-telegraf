06843dc0fe66bd89af0aeeade858326bec56feea
ars <ars.anosov@gmail.com>
2018-04-09 10:28:47 +0300

    клииентская хотелка

--- controllers/tarif_change.js
+++ controllers/tarif_change.js
@@ -7,6 +7,7 @@ const tarifName = {
 }
 // curl -d "request_type=SRGP_API_UPD_TARIF_TYPE&dog_id=01810413042017&tarif_type=9.7" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://89.188.160.0:32180
 
+const nodemailer  = require('nodemailer')
 const fs          = require('fs')
 const path        = require('path')
 
@@ -30,12 +31,51 @@ module.exports = function(ctx, localDb, tarifType, markup) {
 
     if (requestBody === 'Tarif was updated') {
       localDb[ctx.from.id].do.tarif = tarifName[tarifType]
+
       fs.writeFile(path.join(__dirname, '../local_db.json'), JSON.stringify(localDb, "", 2), 'utf8', (err) => {
         if (err) throw err;
         console.log('local_db.json has been saved!')
         ctx.session.value = 'Успешно! Тариф изменен на <b>'+tarifName[tarifType]+'</b>.'
         ctx.editMessageText(ctx.session.value, markup)
       })
+
+      nodemailer.createTestAccount((err, account) => {
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+          host: 'smtp.mail.ru',
+          port: 465,
+          secure: true, // true for 465, false for other ports
+          auth: {
+            user: localDb.smtpData.user,
+            pass: localDb.smtpData.pass
+          }
+        })
+
+        let curDate = new Date()
+        let curDateStr = curDate.toLocaleString("ru", {
+          year: 'numeric',
+          month: 'numeric',
+          day: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric'
+        })
+
+        // setup email data with unicode symbols
+        let mailOptions = {
+          from: '"Telegram Bot" <'+localDb.smtpData.user+'>',
+          to: 'info@srgp.ru',
+          subject: 'Абонент '+ctx.state.role.do.id+' сменил тариф',
+          text: 'Абонент '+ctx.state.role.do.fio+', номер договора '+ctx.state.role.do.id+', телефон '+ctx.state.role.do.phone+'\n\nСменил тарифный план на "'+tarifName[tarifType]+'" '+curDateStr+'.',
+          //html: '<b>Hello world?</b>'
+        }
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) console.log(error)
+          console.log('Message sent: %s', info.messageId);
+        })
+      })
+
     }
     else {
       ctx.session.value = 'Не прошло! SRGP_API_UPD_TARIF_TYPE - тарив не обновился в CRM.'
